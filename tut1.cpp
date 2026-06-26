1c99476f6d1322cc2400b6a6607af9dbcba9fdee
Rupesh <rupeshkumar906960@gmail.com>
2026-06-26 19:12:06 +0530

    files added

---
+++ tut1.cpp
@@ -0,0 +1,10 @@
+#include<iostream>//Header File input output
+
+int main(){
+    std::cout<<"Hello World";
+    return 0;
+}
+
+/* g++ .\tut1.cpp
+   .\a.exe
+*/
---
+++ tut3.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int value = 7;
+    cout<< "Hello World " << value;
+}
